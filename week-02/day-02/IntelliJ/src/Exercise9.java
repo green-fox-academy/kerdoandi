fbbb45f0421bf22cd7007bdff955f3f4a61ba4cb
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 15:26:53 +0100

    Add Exercise9

---
+++ week-02/day-02/IntelliJ/src/Exercise9.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise9 {
+  public static void main(String[] args) {
+
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+
+    double bmi = massInKg / (heightInM * heightInM);
+
+    System.out.println(bmi);
+
+    }
+}
