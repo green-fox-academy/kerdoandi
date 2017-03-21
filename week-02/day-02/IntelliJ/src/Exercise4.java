81a94580421657170103943560ff12d1083719dd
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 14:03:48 +0100

    Add Exercise4

---
+++ week-02/day-02/IntelliJ/src/Exercise4.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise4 {
+  public static void main(String[] args) {
+
+    String name = "Andi";
+    int age = 31;
+    double heightInM = 1.75;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(heightInM);
+  }
+}
