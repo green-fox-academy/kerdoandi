85cc1a9718d8a0b2791283e130e3270a909380fd
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 14:13:28 +0100

    Add Exercise5

---
+++ week-02/day-02/IntelliJ/src/Exercise5.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise5 {
+  public static void main(String[] args) {
+
+    int a = 22;
+    int b = 13;
+
+    System.out.println(a + b);
+    System.out.println(b - a);
+    System.out.println(a * b);
+    System.out.println(22. / 13.);
+    System.out.println(a / b);
+    System.out.println(a % b);
+  }
+}
