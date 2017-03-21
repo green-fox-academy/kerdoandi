2daf966d8c3acf738378ae49448d89e85eebbba3
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 23:50:30 +0100

    Add Exercise24

---
+++ week-02/day-02/IntelliJ/src/Exercise24.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise24 {
+  public static void main(String[] args) {
+
+    for (int i = 2; i < 501; i += 2) {
+      System.out.println(i);
+    }
+  }
+}
