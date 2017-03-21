033e6dd7b6d2a823e4ef0d202b2b868242464caa
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 13:38:46 +0100

    create WSExercises project

--- week-02/day-02/Idea/src/Helloworld.java
+++ week-02/day-02/Idea/src/Helloworld.java
@@ -1,20 +1,26 @@
+import java.util.Scanner;
+
 public class Helloworld {
-    public static void main(String[] args) {
-        System.out.println("alma");
-        System.out.println(123);
-        System.out.println(0.1 + 0.2);
-        System.out.println(true);
+  public static void main(String[] args) {
+    System.out.println("alma");
+    System.out.println(123);
+    System.out.println(0.1 + 0.2);
+    System.out.println(true);
 
-        boolean isMarried = true;
-        String text = "majom";
-        byte b = 127;
-        short peaches = 1;
-        int apples = 123;
-        long pears = 1233;
-        double pi = 3.14;
-        apples = 23;
+    boolean isMarried = true;
+    String text = "majom";
+    byte b = 127;
+    short peaches = 1;
+    int apples = 123;
+    long pears = 1233;
+    double pi = 3.14;
+    apples = 23;
 
-        System.out.println(apples++);
-        System.out.println("alma" == "alma");
-    }
+    System.out.println(apples++);
+    System.out.println("alma" == "alma");
+
+    Scanner scanner = new Scanner(System.in);
+    String line = scanner.nextLine();
+    System.out.println(line);
+  }
 }
