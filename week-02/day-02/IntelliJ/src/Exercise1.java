0156b2ca01b5d03e3b491f38c6eed05c3a1c5587
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 13:51:19 +0100

    Add Exercise1,2 java files

---
+++ week-02/day-02/IntelliJ/src/Exercise1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise1 {
+  public static void main(String[] args) {
+
+      System.out.println("Hello, Andi!");
+  }
+}
+
---
+++ week-02/day-02/IntelliJ/src/Exercise2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise2 {
+  public static void main(String[] args) {
+
+    System.out.println("Hello, Balazs!");
+    System.out.println("Hello, Virag!");
+    System.out.println("Hello, Levi!");
+  }
+}
