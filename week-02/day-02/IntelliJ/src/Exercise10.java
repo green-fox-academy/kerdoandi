e66da5b58d2fdda7820cf14ad18dc62618338a4e
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 15:36:35 +0100

    Add Exercise10

---
+++ week-02/day-02/IntelliJ/src/Exercise10.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise10 {
+  public static void main(String[] args) {
+
+    double a = 15;
+    double b = 20;
+    double c = 45;
+
+    System.out.println("Surface area: " + 2 * (a * b + a * c + b * c));
+    System.out.println("Volume: " + a * b * c);
+  }
+}
