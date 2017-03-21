d329b9d50e93e5568e7152ce3e88aa50e3b8793c
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-21 15:12:24 +0100

    Add Exercise8

---
+++ week-02/day-02/IntelliJ/src/Exercise8.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Andi on 2017. 03. 21..
+ */
+public class Exercise8 {
+  public static void main(String[] args) {
+
+    String name = "Andi";
+    int age = 31;
+    double height = 1.75;
+    boolean isMarried = false;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(isMarried);
+  }
+}
