3abd5aa6afc7378aa3d4b6d6b80bdf84391eec19
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 22:38:20 +0100

    Add AppendA Array

---
+++ week-02/day-03/WorkshopExercises/src/AppendA_Array.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class AppendA_Array {
+  public static void main(String[] args) {
+    String[] animals = {"kuty", "macs", "cic"};
+
+    for (int i = 0; i < animals.length; i++) {
+
+      System.out.print(animals[i] + "a ");
+
+    }
+  }
+}
+
+
+
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
