c2c24fbd971c15d0e4bb8a3f6664a878f5f2960c
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 15:35:30 +0100

    Add Doubling exercise

---
+++ week-02/day-03/WorkshopExercises/src/Doubling.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+
+    System.out.println(doubling(ak));
+  }
+
+  public static int doubling(int ak) {
+
+    return 2 * ak;
+  }
+
+}
+
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
