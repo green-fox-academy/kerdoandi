25d243d9ad929f6dd9b13c11db19deaa6f463fd6
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 14:12:23 +0100

    Add Exercise29, update Exercise28

--- week-02/day-02/IntelliJ/src/Exercise29.java
+++ week-02/day-02/IntelliJ/src/Exercise29.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-public class Exercise28 {
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class Exercise29 {
   public static void main(String[] args) {
     System.out.println("Please enter an even number: ");
 
@@ -8,12 +11,23 @@ public class Exercise28 {
     int row = number.nextInt();
 
     for (int x = 0; x < row; x++) {
-
       for (int y = row - 1 - x; y > 0; y--) {
         System.out.print(" ");
       }
 
       for (int m = (2*x) + 1; m > 0; m--) {
+        System.out.print("*");
+      }
+
+      System.out.println();
+    }
+
+    for (int x = row - 1; x > 0; x--) {
+      for (int y = row - x; y > 0; y--) {
+        System.out.print(" ");
+      }
+
+      for (int m = (2*x) - 1; m > 0; m--) {
         System.out.print("*");
       }
 
