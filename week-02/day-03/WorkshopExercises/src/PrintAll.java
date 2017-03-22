3f0b8eba835bcc6fc69016144d171685f94198b6
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 21:24:32 +0100

    Add PrintAll

---
+++ week-02/day-03/WorkshopExercises/src/PrintAll.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class PrintAll {
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+
+    for (int i = 0; i < af.length; i++) {
+      System.out.println(af[i]);
+    }
+  }
+}
