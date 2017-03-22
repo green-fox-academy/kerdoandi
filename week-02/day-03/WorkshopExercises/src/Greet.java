8bf3dfca1218fb078e64293bc2e6edb07dd36aaa
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 15:46:52 +0100

    Add Greet exercise

---
+++ week-02/day-03/WorkshopExercises/src/Greet.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class Greet {
+  public static void main(String[] args) {
+
+    String al = "Greenfox";
+    greet(al);
+  }
+
+  public static void greet(String name) {
+    System.out.println("Greatings dear, " + name);
+  }
+}
+
+
+//- Create a string variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
