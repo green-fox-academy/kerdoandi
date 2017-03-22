3086403198919bb5e7ca95477061652b19e439de
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-22 23:02:26 +0100

    Add SumAll

---
+++ week-02/day-03/WorkshopExercises/src/SumAll.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Andi on 2017. 03. 22..
+ */
+public class SumAll {
+  public static void main(String[] args) {
+    int sum = 0;
+    int[] ai = {3, 4, 5, 6, 7};
+
+    for (int i = 0; i < ai.length; i++) {
+
+      sum = sum + ai[i];
+
+    }
+    
+    System.out.println(sum);
+  }
+}
+
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai`
