3e95c007ca1f41cb095b923b839f8cb2f04e6dfd
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-23 17:17:22 +0100

    Add IsInList, modify ElementFinder if statement

--- week-02/day-04/Workshop/src/ElementFinder.java
+++ week-02/day-04/Workshop/src/ElementFinder.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 
 public class ElementFinder {
 
-  public static void main(String... args) {
-    ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+  public static void main(String... args){
+    ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
 
     if (arrayList.contains(7)) {
       System.out.println("Hooray");
-    } else {
-      System.out.println("Noooooo");
     }
+    else System.out.println("Noooooo");
   }
 }
 
