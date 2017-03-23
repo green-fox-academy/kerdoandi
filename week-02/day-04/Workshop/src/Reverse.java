ab1f0295dfa442eceae54d52b9d4843b4cd8e2bc
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-23 15:04:38 +0100

    Add Reverse with reversedText function

---
+++ week-02/day-04/Workshop/src/Reverse.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Andi on 2017. 03. 23..
+ */
+public class Reverse {
+  public static void main(String... args) {
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    System.out.println(reversedText(reversed));
+  }
+
+  public static String reversedText(String text) {
+    String result = " ";
+    for ( int i = text.length()-1; i >= 0; i-- ) {
+      result = result + text.charAt(i);
+    }
+
+    return result;
+
+  }
+}
+
+// Create a function that can reverse a String, which is passed as the parameter
+// Use it on this reversed string to check it!
+// Try to solve this using charAt() first, and optionally anything else after.
