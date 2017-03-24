d3cc123d89ddaefe7f771cf4c8cbd17d64c62910
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-24 13:44:08 +0100

    Modify isAnagram function with arraylists

--- week-02/day-05/Demo/src/Anagram.java
+++ week-02/day-05/Demo/src/Anagram.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Anagram {
   public static void main(String[] args) {
@@ -11,16 +14,33 @@ public class Anagram {
 
     System.out.println("The two words are anagrams: " + isAnagram(word1, word2));
 
+
+    System.out.println(isAnagram(word1, word2));
+
   }
 
   public static boolean isAnagram(String word1, String word2) {
 
     if (word1.length() == word2.length()) {
+      ArrayList<Character> charListWord1 = new ArrayList<Character>();
+      ArrayList<Character> charListWord2 = new ArrayList<Character>();
 
       char[] word1ToCharArray = word1.toCharArray();
+      char[] word2ToCharArray = word2.toCharArray();
 
       for (int i = 0; i < word1ToCharArray.length; i++) {
-        if (!word2.contains(String.valueOf(word1ToCharArray[i]))) {
+
+        charListWord1.add(word1ToCharArray[i]);
+      }
+
+      for (int i = 0; i < word2ToCharArray.length; i++) {
+        charListWord2.add(word2ToCharArray[i]);
+      }
+
+      for (int i = 0; i < charListWord1.size(); i++) {
+        if (charListWord2.contains(charListWord1.get(i))) {
+          charListWord2.remove(charListWord1.get(i));
+        } else {
           return false;
         }
       }
