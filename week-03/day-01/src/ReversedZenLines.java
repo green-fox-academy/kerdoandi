031a467757a5b043db7a006e406ffa269a3c1768
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-27 23:39:37 +0200

    Update file with reversed Text

---
+++ week-03/day-01/src/ReversedZenLines.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Andi on 2017. 03. 27..
+ */
+public class ReversedZenLines {
+  public static void main(String[] args) {
+    String filepath = "assets/reversed_zen_lines.txt";
+    List<String> reversedText = new ArrayList<>();
+    reversedText = LottoWithMethods.readFileIntoArrayList(filepath);
+//    System.out.println(reversedText);
+
+
+    for (int i = 0; i < reversedText.size()-1; i++) {
+      char[] text = reversedText.get(i).toCharArray();
+      for (int j = text.length - 1; j > -1; j--) {
+        System.out.print(text[j]);
+      }
+      System.out.println();
+    }
+  }
+}
