8c88071099870ab42338205b6bfcf503ff5e492b
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-27 14:11:02 +0200

    Add Lotto

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -1,5 +1,29 @@
-/**
- * Created by Andi on 2017. 03. 27..
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lotto {
+  public static void main(String[] args) {
+    List<String> lottoLines = new ArrayList<>();
+    
+     try {
+      Path path = Paths.get("assets/lotto.csv");
+      ArrayList<String> lotto = new ArrayList<>(Files.readAllLines(path));
+//      System.out.println(lotto);
+
+      } catch (IOException e)  {
+      System.out.println("error happend: " + e.getClass());
+    }
+
+    for (String oneline : lottoLines)  {
+       String[] splitLines = oneline.split(";");
+      System.out.println(splitLines[11]);
+    }
+  }
 }
+
+
--- week-03/day-01/src/Test.java
+++ week-03/day-01/src/Test.java
@@ -1,17 +1,17 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
-
-public class Test {
-  public static void main(String[] args) {
-  try {
-       Path path = Paths.get("teaching-materials/exercises/file-manipulations/read_lotto_csv/otos.csv");
-    List<String> lines = Files.readAllLines(path);
-  } catch catch (Exception e) {
-      System.out.println("error: " + e.getClass());
-  }
-  }
-}
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+//
+//public class Test {
+//  public static void main(String[] args) {
+//    try {
+//      Path path = Paths.get("teaching-materials/exercises/file-manipulations/read_lotto_csv/otos.csv");
+//      List<String> lines = Files.readAllLines(path);
+//    } catch (Exception e) {
+//      System.out.println("error: " + e.getClass());
+//    }
+//  }
+//}
