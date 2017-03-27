925594bb9b8dfe7460b8b3c624e36d3c271bb4c5
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-27 15:18:33 +0200

    Add for each to oneline Strings

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -1,20 +1,32 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Andi on 2017. 03. 27..
  */
 public class Doubled {
   public static void main(String[] args) {
+    List<String> text = new ArrayList<>();
+
     try {
       Path path = Paths.get("assets/duplicated_chars.txt");
-      ArrayList<String> text = new ArrayList<>(Files.readAllLines(path));
-      System.out.println(text);
-    } catch (Exception e){
+      text = Files.readAllLines(path);
+    } catch (IOException e) {
       System.out.println("error");
+    }
+    System.out.println(text);
+
+    List<String> nonDoubledText = new ArrayList<>();
+    for (String oneline : text) {
+
+      if (oneline.length() != 0) {
+
+      }
     }
   }
 }
