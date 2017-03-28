5f44965e30cb0cf4dfae7ea353ef9df2a957c9ac
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-28 14:46:13 +0200

    Create Function Class to collect useful functions for future uses

---
+++ week-03/day-01/src/Functions.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Andi on 2017. 03. 28..
+ */
+public class Functions {
+
+}public static List<String> readFileIntoArrayList(String filepath)  {
+  List<String> arrayListFromFile = new ArrayList<>();
+  try {
+    Path path = Paths.get(filepath);
+    arrayListFromFile = Files.readAllLines(path);
+
+  } catch (IOException e) {
+    System.out.println("error happend: " + e.getClass());
+  }
+  return arrayListFromFile;
+}
