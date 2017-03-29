8372738eb243e08bf9d855d342975fe3194d047e
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-29 11:45:04 +0200

    Add Position Square with random coordinates

---
+++ week-03/day-03/src/W08_PositionSquare.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class W08_PositionSquare {
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    Random rand = new Random();
+    
+    for (int i = 0; i < 3; i++) {
+      int x = rand.nextInt(250);
+      int y = rand.nextInt(250);
+      drawSquare(graphics, x, y);
+    }
+
+
+  }
+
+  public static void drawSquare(Graphics g, int x, int y) {
+    int width = 50;
+    int height = 50;
+    g.drawRect(x,y, width, height);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+
+}
