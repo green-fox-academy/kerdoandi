7ed0344239216fa902f60341fd5118a7e7d14973
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-29 16:45:06 +0200

    Add W16_ LPQuarters exercise

---
+++ week-03/day-03/src/W16_LinePlayQuarters.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+
+public class W16_LinePlayQuarters {
+  public static void mainDraw(Graphics graphics){
+    // divide the canvas into 4 parts
+    // and repeat this pattern in each quarter:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/line-play/r1.png]
+  graphics.setColor(Color.blue);
+  graphics.drawLine(150,0,150,300);
+  graphics.drawLine(0, 150, 300,150);
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
