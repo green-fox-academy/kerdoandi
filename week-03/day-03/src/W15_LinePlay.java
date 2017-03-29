67165db7f59a3380500515c3f664c1910502479b
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-29 16:38:47 +0200

    Add final W15

--- week-03/day-03/src/W15_LinePlay.java
+++ week-03/day-03/src/W15_LinePlay.java
@@ -9,23 +9,25 @@ public class W15_LinePlay {
   public static void mainDraw(Graphics graphics){
     // reproduce this:
     // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/line-play/r1.png]
-    int x = 20;
-    int y = 30;
-
-    drawLinePlay(graphics, x,y);
-
-  }
-
-  public static void drawLinePlay (Graphics g, int x, int y) {
+    int x;
+    int y;
     int space = 20;
 
     for (int i = 0; i < 16; i++) {
-      x = i * space;
-      y = 300 - (i * space);
-      g.setColor(Color.BLUE);
-      g.drawLine(x, 0, 300, y);
+      x = 1+ i * space;
+      y = 1+ i * space;
+      graphics.setColor(Color.BLUE);
+      graphics.drawLine(x, 0, 300, y);
+    }
+
+    for (int i = 0; i < 16; i++) {
+      x = 1 + i * space;
+      y = 1 + i * space;
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(0, y, x, 300);
     }
   }
+
 
   //    Don't touch the code below
   public static void main(String[] args) {
