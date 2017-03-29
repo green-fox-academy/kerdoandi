3672a1b2c3b5c62372cbc17d1fe889649946f461
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-29 23:54:02 +0200

    Create StarryNight.java

--- week-03/day-03/src/W19_StarryNight.java
+++ week-03/day-03/src/W19_StarryNight.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -11,9 +12,18 @@ public class W19_StarryNight {
     // - The stars can be small squares
     // - The stars should have random positions on the canvas
     // - The stars should have random color (some shade of grey)
+    graphics.setColor(Color.BLACK);
+    graphics.fillRect(0,0,300,300);
 
+    Random rand = new Random();
+    int size = 2;
+    int x = rand.nextInt(298) + 1;
+    int y = rand.nextInt(298) + 1;
 
-
+    for (int i = 0; i < 150; i++) {
+      graphics.setColor(Color.WHITE);
+      graphics.fillRect(x,y,size, size);
+    }
   }
 
   //    Don't touch the code below
