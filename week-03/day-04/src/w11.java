27410e0eedddc3c5ef63d31de94a37938db13f92
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-30 17:34:21 +0200

    Create W11.java

--- week-03/day-04/src/w11.java
+++ week-03/day-04/src/w11.java
@@ -8,13 +8,28 @@ public class w11 {
   public static void mainDraw(Graphics graphics){
     int canvasSize = 300;
 
-    graphics.setColor(Color.BLACK);
-    graphics.drawRect(0,0,canvasSize,canvasSize);
-    graphics.drawRect(canvasSize/3,0,canvasSize/3,canvasSize/3);
-    graphics.drawRect(2 * (canvasSize/3),canvasSize/3,canvasSize/3,canvasSize/3);
-    graphics.drawRect(canvasSize/3,2 * (canvasSize/3),canvasSize/3,canvasSize/3);
-    graphics.drawRect(0,canvasSize/3,canvasSize/3,canvasSize/3);
 
+
+  }
+
+  public static void toMultiplyBoxes(Graphics g, int i) {
+    int canvasSize = 300;
+
+    if (i == 0) {
+      g.setColor(Color.BLACK);
+      g.drawRect(0, 0, canvasSize, canvasSize);
+      return toMultiplyBoxes(g, i);
+
+    } else {
+      canvasSize = canvasSize/3;
+
+      g.drawRect(canvasSize , 0, canvasSize, canvasSize);
+      g.drawRect(2 * canvasSize, canvasSize, canvasSize, canvasSize );
+      g.drawRect(canvasSize, 2 * canvasSize, canvasSize, canvasSize);
+      g.drawRect(0, canvasSize, canvasSize, canvasSize);
+
+      return toMultiplyBoxes(g,i -1);
+    }
   }
 
   //    Don't touch the code below
