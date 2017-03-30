ce540c299de4198ee56d850b3bd6c950bff296b9
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-30 13:53:25 +0200

    Add StringAgain ready

---
+++ week-03/day-04/src/w08StringsAgain.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Andi on 2017. 03. 30..
+ */
+public class w08StringsAgain {
+  public static void main(String[] args) {
+    String text = "kaaaaxxkkaaaxxxxx";
+    System.out.println(toRemoveX(text));
+  }
+
+  public static String toRemoveX(String text) {
+    char remove = 'x';
+
+    if (text.length()<1) {
+      return text;
+
+    } else{
+
+      if (text.charAt(0) == remove) {
+        return toRemoveX(text.substring(1));
+      } else {
+        return text.charAt(0) + toRemoveX(text.substring(1));
+      }
+    }
+  }
+}
