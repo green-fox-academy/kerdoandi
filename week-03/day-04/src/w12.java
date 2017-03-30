53ef0d8e9e53f1b6ae326738b5de20e28e036898
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-30 22:20:34 +0200

    Setup w12.java

---
+++ week-03/day-04/src/w12.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+//public class w12 {
+//  public static void mainDraw(Graphics graphics) {
+//
+//
+//  }
+//  public static void toDrawHex(Graphics g, int x, int y, int size, int i) {
+//  int[] = {}
+//
+//
+//  }
+//
+//
+//
+//  //    Don't touch the code below
+//  public static void main(String[] args) {
+//    JFrame jFrame = new JFrame("Drawing");
+//    jFrame.setSize(new Dimension(300, 300));
+//    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//    jFrame.add(new ImagePanel());
+//    jFrame.setLocationRelativeTo(null);
+//    jFrame.setVisible(true);
+//  }
+//
+//  static class ImagePanel extends JPanel {
+//    @Override
+//    protected void paintComponent(Graphics graphics) {
+//      super.paintComponent(graphics);
+//      mainDraw(graphics);
+//
+//    }
+//  }
+//
+//}
+//
+//
