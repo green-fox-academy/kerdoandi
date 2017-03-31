bd72f62e4b7b5b0588f004640e6bf212a39cd9de
Andrea Kerdo <kerdoandi@gmail.com>
2017-03-31 10:46:49 +0200

    Add circles ready

---
+++ week-03/day-05/src/Circles.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Circles {
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(Color.BLACK);
+    toMultiplyOvals(graphics, 0, 0, 300);
+  }
+
+  public static void toMultiplyOvals(Graphics g, int x, int y, int size) {
+    g.drawOval(x, y, size, size);
+
+    if (size < 60) {
+      return;
+    } else {
+      toMultiplyOvals(g,x +size/4, y, size/2);
+      toMultiplyOvals(g, x+size/16, y + 4*size/10, size/2);
+      toMultiplyOvals(g, x+ 7*size/16,y + 4*size/10, size/2);
+    }
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new Circles.ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      this.setBackground(Color.YELLOW);
+      mainDraw(graphics);
+
+    }
+  }
+}
