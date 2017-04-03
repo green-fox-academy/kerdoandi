e9d64e2362397b33301a99bc8f6350c6861402ab
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-03 11:33:01 +0200

    Add BlogPost project

---
+++ week-04/day-01/Project02_BlogPost/src/BlogPost.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Andi on 4/3/2017.
+ */
+public class BlogPost {
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+}
