cb1e44508660eb9ad344469c5c9106be881f007c
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-03 11:21:51 +0200

    Create PostIt project, ready

---
+++ week-04/day-01/Project01_PostIt/src/PostIt.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Andi on 4/3/2017.
+ */
+public class PostIt {
+
+  String backgroundcolor;
+  String textOnIt;
+  String textColor;
+
+  public PostIt (String a, String b, String c) {
+    backgroundcolor = a;
+    textOnIt = b;
+    textColor = c;
+  }
+}
---
+++ week-04/day-01/Project01_PostIt/src/PostItObjects.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andi on 4/3/2017.
+ */
+public class PostItObjects {
+  public static void main(String[] args) {
+
+    PostIt version1 = new PostIt("orange", "Idea 1", "blue");
+    PostIt version2 = new PostIt("pink", "Awesome", "black");
+    PostIt version3 = new PostIt("yellow", "Superb!", "green");
+
+  }
+}
