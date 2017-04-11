4b6c6661e6869732936a35b6d02822b5d37d20eb
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-11 16:10:56 +0200

    Update hero movement, hero can't cross the wall

--- week-05/day-02/src/Board.java
+++ week-05/day-02/src/Board.java
@@ -4,10 +4,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Board extends JComponent implements KeyListener {
+  int testBoxX = 0;
+  int testBoxY = 0;
+  public static final int DIMENSION = 72;
 
-  int testBoxX;
-  int testBoxY;
-  static final int DIMENSION = 72;
   int[][] tilesMap = new int[][]{
           {0, 0, 0, 1, 0, 1, 0, 0, 0, 0,},
           {0, 0, 0, 1, 0, 1, 0, 1, 1, 0,},
@@ -23,8 +23,6 @@ public class Board extends JComponent implements KeyListener {
   };
 
   public Board() {
-    testBoxX = 0;
-    testBoxY = 0;
     setPreferredSize(new Dimension(DIMENSION * 10, DIMENSION * 11));
     setVisible(true);
   }
@@ -32,43 +30,27 @@ public class Board extends JComponent implements KeyListener {
   @Override
   public void paint(Graphics graphics) {
     super.paint(graphics);
+    PositionedImage hero = new PositionedImage("pictures/hero-down.png", testBoxX, testBoxY);
+
     int posX = 0;
     int posY = 0;
     for (int i = 0; i < 11; i++) {
       for (int j = 0; j < 10; j++) {
         if (tilesMap[i][j] == 0) {
-          PositionedImage image = new PositionedImage("pictures/floor.png", posX, posY);
-          image.draw(graphics);
+          PositionedImage floor = new PositionedImage("pictures/floor.png", posX, posY);
+          floor.draw(graphics);
         } else {
-          PositionedImage image = new PositionedImage("pictures/wall.png", posX, posY);
-          image.draw(graphics);
+          PositionedImage wall = new PositionedImage("pictures/wall.png", posX, posY);
+          wall.draw(graphics);
         }
         posX += DIMENSION;
       }
-      posX= 0;
+      posX = 0;
       posY += DIMENSION;
     }
+    hero.draw(graphics);
   }
 
-
-
-  public static void main(String[] args) {
-    // Here is how you set up a new window and adding our board to it
-    JFrame frame = new JFrame("RPG Game");
-    Board board = new Board();
-    frame.add(board);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-    frame.pack();
-    // Here is how you can add a key event listener
-    // The board object will be notified when hitting any key
-    // with the system calling one of the below 3 methods
-    frame.addKeyListener(board);
-    // Notice (at the top) that we can only do this
-    // because this Board class (the type of the board object) is also a KeyListener
-  }
-
-  // To be a KeyListener the class needs to have these 3 methods in it
   @Override
   public void keyTyped(KeyEvent e) {
 
@@ -79,20 +61,25 @@ public class Board extends JComponent implements KeyListener {
 
   }
 
-  // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
-    // When the up or down keys hit, we change the position of our box
-    if (e.getKeyCode() == KeyEvent.VK_UP) {
-      testBoxY -= 72;
-    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-      testBoxY += 72;
-    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-      testBoxX -= 72;
-    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      testBoxX += 72;
-      // and redraw to have a new picture with the new coordinates
-      repaint();
+    if (e.getKeyCode() == KeyEvent.VK_UP && testBoxY > 0) {
+      if (tilesMap[(testBoxY/72) -1][testBoxX/72] != 1) {
+        testBoxY -= 72;
+      }
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN && testBoxY < 720) {
+      if (tilesMap[(testBoxY/72) + 1][testBoxX/72] != 1) {
+        testBoxY += 72;
+      }
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT && testBoxX > 0) {
+      if (tilesMap[testBoxY/72][(testBoxX/72) -1] != 1) {
+        testBoxX -= 72;
+      }
+    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && testBoxX < (720 - 72)) {
+      if (tilesMap[testBoxY/72][(testBoxX/72) + 1] != 1) {
+        testBoxX += 72;
+      }
     }
+    repaint();
   }
 }
