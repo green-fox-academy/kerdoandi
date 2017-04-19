ae625f7fa80c6203de96862703300690a7387281
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-19 16:56:52 +0200

    Add SortArray

---
+++ week-06/day-03/SortTheArray/src/SortTheArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SortTheArray {
+  public static void main(String[] args) {
+    Integer[] intArray = {4,6,2,6,8,0,76,2,6};
+    Double[] doubleArray = {1.5, 6.7, 8.5, 9.5, 2.5, 7.1};
+    toDescendArray(intArray);
+    toDescendArray(doubleArray);
+    System.out.println(Arrays.toString(intArray));
+    System.out.println(Arrays.toString(doubleArray));
+  }
+
+  public static <T extends Number> void toDescendArray(T[] array) {
+    Arrays.sort(array);
+  }
+}
