94b2b117a97c47e2c61d317085d885ea15320822
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-20 14:36:34 +0200

    Add calculateAgeinyears

--- week-06/day-04/birthdaycalculator/src/main/java/date/BirthdayWithLocalDate.java
+++ week-06/day-04/birthdaycalculator/src/main/java/date/BirthdayWithLocalDate.java
@@ -1,36 +1,48 @@
 package date;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+
+import static java.lang.Math.toIntExact;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class BirthdayWithLocalDate implements date.BirthdayCalculator<LocalDate> {
 
   @Override
   public LocalDate parseDate(String str) {
-    // TODO - return with the parsed date; format is: yyyy-MM-dd
+    LocalDate localDate = LocalDate.parse(str);
+    return localDate;
   }
 
   @Override
   public String printMonthAndDay(LocalDate date) {
-    // TODO - return the date formatted: month & day (MM. dd.)
+    String monthAndDay = date.toString().substring(5,7) + ". " + date.toString().substring(8) + ".";
+    return monthAndDay;
   }
+  // TODO - return the date formatted: month & day (MM. dd.)
 
   @Override
   public boolean isAnniversaryToday(LocalDate date) {
-    // TODO - return with true if today is the same month+day as date
-  }
+    return (date.getMonth() == LocalDate.now().getMonth() && date.getDayOfMonth() == LocalDate.now().getDayOfMonth());
+}
 
   @Override
   public int calculateAgeInYears(LocalDate birthday) {
-    // TODO - return how many years age the input date 'birthday' was
+    int age = LocalDate.now().getYear() - birthday.getYear();
+    return age;
   }
+  // TODO - return how many years age the input date 'birthday' was
 
   @Override
   public int calculateDaysToNextAnniversary(LocalDate date) {
-    // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
+    Period periodToNextAnniversary = Period.between(date, LocalDate.now());
+    int remainingDays = periodToNextAnniversary.getDays();
+    return remainingDays;
   }
+  // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
 
   public static void main(String[] args) {
     new BirthdayWithLocalDate().run();
