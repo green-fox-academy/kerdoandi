e20b96c698bbef33d866892da6eaaaff70eb3ede
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-27 10:57:36 +0200

    Add Comparable to Student class

---
+++ week-06/day-02/TeachersAndStudentsComparator/src/main/java/GreenfoxApp.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Andi on 4/4/2017.
+ */
+public class GreenfoxApp {
+  public static void main(String[] args) {
+    ArrayList<Person> people = new ArrayList<>();
+
+    Person mark = new Person("Mark", 46, "male");
+    people.add(mark);
+    Person jane = new Person();
+    people.add(jane);
+    Student john = new Student("John Doe", 20, "male", "BME");
+    people.add(john);
+    Student anne = new Student("Anne", 20, "female", "BME");
+    anne.setSkippedDays(5);
+    people.add(anne);
+    Student student = new Student();
+    people.add(student);
+    Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
+    people.add(gandhi);
+    Mentor mentor = new Mentor();
+    people.add(mentor);
+    Sponsor sponsor = new Sponsor();
+    Sponsor elon = new Sponsor("Elon Musk", 46, "male", "SpaceX");
+
+    student.skipDays(3);
+
+    for (int i = 0; i < 5; i++) {
+      elon.hire();
+    }
+    for (int i = 0; i < 3; i++) {
+      sponsor.hire();
+    }
+
+    for (Person person : people) {
+      person.introduce();
+      person.getGoal();
+    }
+
+    Lagopus badass = new Lagopus("BADA55");
+    badass.addStudent(student);
+    badass.addStudent(john);
+    badass.addMentor(mentor);
+    badass.addMentor(gandhi);
+    badass.info();
+    System.out.println(john.compareTo(anne));
+  }
+}
---
+++ week-06/day-02/TeachersAndStudentsComparator/src/main/java/Student.java
@@ -0,0 +1,40 @@
+public class Student extends Person implements Comparable<Student> {
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  @Override
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.getName() + ", a " + this.getAge() + "year old " + this.getGender() + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+
+  public Student(String name, int age, String gender, String po) {
+    super(name, age, gender);
+    this.previousOrganization = po;
+    this. skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    this.previousOrganization = "The School of Life";
+    this. skippedDays = 0;
+  }
+
+  @Override
+  public int compareTo(Student o) {
+    return this.skippedDays - o.skippedDays;
+  }
+
+  public void setSkippedDays(int skippedDays) {
+    this.skippedDays = skippedDays;
+  }
+}
