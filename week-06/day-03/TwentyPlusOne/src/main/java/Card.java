a982090597312c3bd8dce25c90358974202eab63
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-30 17:57:06 +0200

    Add TwentyPlusOne project

---
+++ week-06/day-03/TwentyPlusOne/src/main/java/Card.java
@@ -0,0 +1,21 @@
+public class Card {
+  enum CardColor {RED, BLACK}
+  enum Suit {CLUBS, DIAMONDS, HEARTS, SPADES}
+  enum Rank {
+    ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+    private final int value;
+
+    Rank(int value) {
+      this.value = value;
+    }
+  }
+    CardColor cardColor;
+    Suit suit;
+    Rank rank;
+
+  public Card(CardColor cardColor, Suit suit, Rank rank) {
+    this.cardColor = cardColor;
+    this.suit = suit;
+    this.rank = rank;
+  }
+}
