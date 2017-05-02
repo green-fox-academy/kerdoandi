6559e864927ee8420a2cc4b924b3cb15f495efe5
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-02 12:22:03 +0200

    Add RequestParam

--- week-07/day-02/springstart/src/main/java/com/greenfox/controller/HelloRESTController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfox/controller/HelloRESTController.java
@@ -2,12 +2,13 @@ package com.greenfox.controller;
 
 import com.greenfox.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
   @RequestMapping (value = "/greeting")
-  public Greeting greeting() {
-    return new Greeting(1, "Hello, World");
+  public Greeting greeting(@RequestParam String name) {
+    return new Greeting(1, String.format("Hello, %s", name));
   }
 }
