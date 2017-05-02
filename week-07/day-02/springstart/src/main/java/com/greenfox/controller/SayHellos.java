1136a96d0f4e085a9073a2673d80ea8768b63472
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-02 14:48:05 +0200

    Add SayHellos

---
+++ week-07/day-02/springstart/src/main/java/com/greenfox/controller/SayHellos.java
@@ -0,0 +1,23 @@
+package com.greenfox.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Random;
+
+
+@Controller
+public class SayHellos {
+  @RequestMapping("/web/greetingv3")
+  public String greeting(Model model) {
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    Random random = new Random();
+    String tempGreet = hellos[random.nextInt(hellos.length)];
+    model.addAttribute("greeting", tempGreet);
+    return "greetinglanguages";
+  }
+}
