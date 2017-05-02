d2ad4ec389858142ebab10e071068e8e9dd4c192
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-02 13:39:12 +0200

    Add HelloWebApp

--- week-07/day-02/springstart/src/main/java/com/greenfox/controller/HelloRESTController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfox/controller/HelloRESTController.java
@@ -1,14 +1,14 @@
 package com.greenfox.controller;
 
 import com.greenfox.Greeting;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 
-@RestController
+@Controller
 public class HelloRESTController {
   AtomicLong id = new AtomicLong();
   @RequestMapping (value = "/greeting")
