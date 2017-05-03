6df95bf6de5c3d96eb1185410628ef760a788a00
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-03 12:26:13 +0200

    Add Exercise3

--- week-07/day-03/lionking/src/main/java/com/greenfox/Controler/Exercises.java
+++ week-07/day-03/lionking/src/main/java/com/greenfox/Controler/Exercises.java
@@ -9,11 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Exercises {
   BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
-  @RequestMapping("/exercise2")
+  @RequestMapping("/exercise1")
   public String listFields(Model model) {
     model.addAttribute("name", bankAccount.getName());
     model.addAttribute("balance", bankAccount.getBalance());
     model.addAttribute("type", bankAccount.getType());
     return "bankAccount";
+  }
+
+  @RequestMapping("/exercise2")
+  public String formatDecimals(Model model) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("type", bankAccount.getType());
+    return "bankAccount02";
+  }
+
+  @RequestMapping("/exercise3")
+  public String addZebra(Model model) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("type", bankAccount.getType());
+    return "bankAccount03";
   }
 }
