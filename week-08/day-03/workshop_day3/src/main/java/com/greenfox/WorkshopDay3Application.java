de9040d8107befd4af0a673964e95dba9de4e9d8
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-10 11:35:38 +0200

    Add Exercise1

--- week-08/day-03/workshop_day3/src/main/java/com/greenfox/WorkshopDay3Application.java
+++ week-08/day-03/workshop_day3/src/main/java/com/greenfox/WorkshopDay3Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontendApplication {
+public class WorkshopDay3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrontendApplication.class, args);
+		SpringApplication.run(WorkshopDay3Application.class, args);
 	}
 }
--- week-08/day-03/workshop_day3/src/test/java/com/greenfox/WorkshopDay3ApplicationTests.java
+++ week-08/day-03/workshop_day3/src/test/java/com/greenfox/WorkshopDay3ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FrontendApplicationTests {
+public class WorkshopDay3ApplicationTests {
 
 	@Test
 	public void contextLoads() {
