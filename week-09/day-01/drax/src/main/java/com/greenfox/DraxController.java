d1e201b4694d8c93384cfecb2bc805fb8dc2dfa4
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-15 17:22:37 +0200

    Add delete and change endpoints

--- week-09/day-01/drax/src/main/java/com/greenfox/DraxController.java
+++ week-09/day-01/drax/src/main/java/com/greenfox/DraxController.java
@@ -2,19 +2,34 @@ package com.greenfox;
 
 import com.greenfox.model.Food;
 import com.greenfox.model.Foods;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DraxController {
+  @Autowired
+  Foods foods;
 
   @RequestMapping(value = "/drax", method = RequestMethod.GET)
   public Foods getFoodList() {
-    Foods foods = new Foods();
-    foods.addFood(new Food("apple", 1, 250));
-    foods.addFood(new Food("bread", 1, 600));
+    return foods;
+  }
+
+  @RequestMapping(value = "/drax/add", method = RequestMethod.POST)
+  public Foods addFood(@RequestBody Food food) {
+    foods.addFood(food);
+    return foods;
+  }
+
+  @RequestMapping(value = "/drax/remove", method = RequestMethod.DELETE)
+  public Foods removeFood(@RequestParam(value="food") String name) {
+    foods.removeFood(name);
+    return foods;
+  }
+
+  @RequestMapping(value = "/drax/changeamount", method = RequestMethod.PUT)
+  public Foods changeFood(@RequestParam(value="food") String name, @RequestParam(value="amount") double amount) {
+    foods.changeFoodAmount(name, amount);
     return foods;
   }
 }
--- week-09/day-01/drax/src/main/java/com/greenfox/model/Foods.java
+++ week-09/day-01/drax/src/main/java/com/greenfox/model/Foods.java
@@ -1,7 +1,10 @@
 package com.greenfox.model;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class Foods {
   List<Food> foods;
 
@@ -19,5 +22,21 @@ public class Foods {
 
   public void addFood(Food food) {
     foods.add(food);
+  }
+
+  public void removeFood(String name) {
+    for (Food food : foods) {
+      if (food.getName().equals(name)) {
+        foods.remove(food);
+      }
+    }
+  }
+
+  public void changeFoodAmount(String name, double amount) {
+    for (Food food : foods) {
+      if (food.getName().equals(name)) {
+        food.setAmount(amount);
+      }
+    }
   }
 }
