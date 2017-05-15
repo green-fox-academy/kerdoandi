2e42dd2fb52585399023d96cb2affc8a242268fe
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-15 12:30:50 +0200

    Add Test method with query param

--- week-09/day-01/groot/src/main/java/com/greenfox/GuardianController.java
+++ week-09/day-01/groot/src/main/java/com/greenfox/GuardianController.java
@@ -1,15 +1,19 @@
 package com.greenfox;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GuardianController {
 
   @RequestMapping(value = "/groot", method =RequestMethod.GET)
-  public Groot getGroot(@RequestParam(value = "message") String received) {
+  public Groot getGroot(@RequestParam(value = "message", required = true) String received) {
     Groot groot = new Groot(received);
     return groot;
+  }
+
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ErrorMessage parameterException(MissingServletRequestParameterException e) {
+    String parameter = e.getParameterName();
+    return new ErrorMessage("I am groot!");
   }
 }
--- week-09/day-01/groot/src/test/java/com/greenfox/GrootApplicationTests.java
+++ week-09/day-01/groot/src/test/java/com/greenfox/GrootApplicationTests.java
@@ -1,16 +1,49 @@
 package com.greenfox;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = GrootApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
 public class GrootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+					MediaType.APPLICATION_JSON.getSubtype(),
+					Charset.forName("utf8"));
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+}
+
+	@Test
+	public void GrootGreetWithQueryParam() throws Exception {
+    mockMvc.perform(get("/groot?message=somemessage"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.received").value("somemessage"))
+            .andExpect(jsonPath("$.translated").value("I am Groot!"));
+  }
 }
