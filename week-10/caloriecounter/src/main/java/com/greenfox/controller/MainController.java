8ebe23158700b83229e00ccbbc809a3dd2dd9d7d
Kerdo Andrea <kerdoandi@gmail.com>
2017-06-01 15:22:19 +0200

    Update Controller

--- week-10/caloriecounter/src/main/java/com/greenfox/controller/MainController.java
+++ week-10/caloriecounter/src/main/java/com/greenfox/controller/MainController.java
@@ -6,9 +6,7 @@ import com.greenfox.repository.MealsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/meal")
@@ -36,10 +34,7 @@ public class MainController {
 
   @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
   public String deleteMeal(Model model, @PathVariable(value = "id") long id) {
-    System.out.println("id= " + id);
-    System.out.println("torles elott");
     mealsRepository.delete(id);
-    System.out.println("torles utan");
     return "redirect:/meal/";
   }
 }
