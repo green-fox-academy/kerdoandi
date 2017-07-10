34e46f918c2f9e936caad3a575c47ec1b9e2e8b8
Kerdo Andrea <kerdoandi@gmail.com>
2017-07-10 09:42:48 +0200

    try out the return array method

--- week-16/finalExam20170710/src/Exercise12.java
+++ week-16/finalExam20170710/src/Exercise12.java
@@ -1,7 +1,13 @@
-public class Exercise12 {
+import java.util.Arrays;
 
+public class Exercise12 {
   public static void main(String[] args) {
-    
+    int[][] matrix = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+    };
+    System.out.println(Arrays.toString(returnSumOfEachRow(matrix)));
   }
 
   public static int[] returnSumOfEachRow(int[][] matrix) {
